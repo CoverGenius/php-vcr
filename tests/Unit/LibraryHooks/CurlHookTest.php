a71f4a944e33241c4e6a0f12a5ec89c4992064de
Grant Ozolins <grant.o@covergenius.com>
2026-07-15 17:19:19 +1000

    fix phpcs report: use arrow function in test

--- tests/Unit/LibraryHooks/CurlHookTest.php
+++ tests/Unit/LibraryHooks/CurlHookTest.php
@@ -444,9 +444,7 @@ final class CurlHookTest extends TestCase
     public function testCurlMultiAddHandleReturnsCurlmOk(): void
     {
         $this->curlHook->enable(
-            function (Request $request): Response {
-                return new Response('200');
-            }
+          fn (Request $request): Response => new Response('200')
         );
 
         $curlHandle = curl_init('http://example.com');
